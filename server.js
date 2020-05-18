b63b81173a78cf616513df0e720d5fe4444ab285
niccoryan0 <niccoryan0@gmail.com>
2020-05-18 13:44:55 -0700

    started scaffold

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Packages
+const express = require('express');
+const cors = require('cors');
+
+// Global vars
+const PORT = process.env.PORT;
+const app = express();
+
+// Config
+app.use(cors());
+
+// Routes
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, `Listening on  ${PORT}`);
