0da69b7a956e029a4092b74db51c615e796260c0
niccoryan0 <niccoryan0@gmail.com>
2020-05-18 20:23:01 -0700

    basic site done ;

--- server.js
+++ server.js
@@ -3,25 +3,31 @@
 // Packages
 const express = require('express');
 const cors = require('cors');
+var bodyParser = require('body-parser');
+var path = require('path');
+require('dotenv').config();
+
+
 
 // Global vars
 const PORT = process.env.PORT;
 const app = express();
+app.use(express.static('./public')); //helps the frontend
 
 // Config
 app.use(cors());
 
 // Routes
+// app.set('views', path.join(__dirname, 'views') );
+app.set('view engine', 'ejs');
+
+app.get('/hello', (req,res) => {
+  res.render('index');
+});
 
 
 
 
 
 
-
-
-
-
-
-
-app.listen(PORT, `Listening on  ${PORT}`);
+app.listen(PORT, console.log(`Listening on  ${PORT}`));
